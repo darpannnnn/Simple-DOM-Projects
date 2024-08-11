430fdb1b3ff282e0fce9dc6815dbb48c95b6d611
Darpan Kambli <darpankambli006@gmail.com>
2024-08-11 13:17:37 +0530

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,9 +1,17 @@
 //bellow is the code for menu btn functioning
 const menuBtn = document.getElementById('menuBtn');
 const dropdownMenu = document.querySelector('.dropdownMenu');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click',function() {
     dropdownMenu.classList.toggle('display');
+});
+
+// Close the dropdown menu when clicking anywhere outside of it
+body.addEventListener('click', function() {
+    if (dropdownMenu.classList.contains('display')) {
+        dropdownMenu.classList.remove('display');
+    }
 });
 
 //reset btn
